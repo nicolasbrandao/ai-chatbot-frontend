b8ba888b8c4197db93006a394a7a845ab41ead13
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-30 17:26:46 -0300

    ref: refactor to google provider

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -5,17 +5,16 @@ import { signIn } from 'next-auth/react'
 
 import { cn } from '@/lib/utils'
 import { Button, type ButtonProps } from '@/components/ui/button'
-import { IconGitHub, IconSpinner } from '@/components/ui/icons'
+import { IconGoogle, IconSpinner } from '@/components/ui/icons'
 
 interface LoginButtonProps extends ButtonProps {
-  showGithubIcon?: boolean
+  showGoogleIcon?: boolean
   text?: string
 }
 
-// TODO: Change this to google provider
 export function LoginButton({
-  text = 'Login with GitHub',
-  showGithubIcon = true,
+  text = 'Login with Google',
+  showGoogleIcon = true,
   className,
   ...props
 }: LoginButtonProps) {
@@ -26,7 +25,7 @@ export function LoginButton({
       onClick={() => {
         setIsLoading(true)
         // next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
-        signIn('github', { callbackUrl: `/` })
+        signIn('google', { callbackUrl: `/` })
       }}
       disabled={isLoading}
       className={cn(className)}
@@ -34,8 +33,8 @@ export function LoginButton({
     >
       {isLoading ? (
         <IconSpinner className="mr-2 animate-spin" />
-      ) : showGithubIcon ? (
-        <IconGitHub className="mr-2" />
+      ) : showGoogleIcon ? (
+        <IconGoogle className="mr-2" />
       ) : null}
       {text}
     </Button>
