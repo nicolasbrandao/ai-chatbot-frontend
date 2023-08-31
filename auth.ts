6786e42cc26afc1ac8dbcedb5ac7523c9591f1d0
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-30 21:20:46 -0300

    fea: use google auth provider

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth, { type DefaultSession } from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 
 declare module 'next-auth' {
   interface Session {
@@ -15,7 +15,7 @@ export const {
   auth,
   CSRF_experimental // will be removed in future
 } = NextAuth({
-  providers: [GitHub],
+  providers: [Google],
   callbacks: {
     jwt({ token, profile }) {
       if (profile) {
