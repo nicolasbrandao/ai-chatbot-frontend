b3a0bdb181259da927227eb4fddfddd2db84d207
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-08-31 00:04:30 -0300

    fix: add default export

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -2,6 +2,8 @@
 
 import { ImageLoader } from "next/image"
 
-export const imageLoader: ImageLoader = ({ src, width, quality }) => {
+const imageLoader: ImageLoader = ({ src, width, quality }) => {
   return `/_next/image?url=${src}?w=${width}&q=${quality || 75}`
 }
+
+export default imageLoader
