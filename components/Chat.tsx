dfd016213c9f09407e0724f3618c6c42ead5e5e9
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-09-02 18:32:03 -0300

    Refactor Chat functionality and improve interaction with API
    This commit introduces several important changes to the Chat component:
    
    - Added a loading state to provide feedback to the user while the chat is waiting for an AI response, it's represented by a spinner
    - Refactored the API fetching related code for better error handling
    - Now the component reads data in chunks and provides real-time AI response, leading to a smoother conversation
    - Replaced usage of local api urls with absolute urls to fetch chat history and interact with the chat API
    - Updated the handleSubmit function to reflect the changes made in how the chat API is being interacted, now incorporating a while loop for continuous reading of API response
    - Minor adjustments are also made to improve the user interaction, such as disabling send button while the request is loading.

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -6,63 +6,78 @@ import Image from "next/image";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 export default function Chat() {
-  const [chat, setChat] = useState<Message[]>([])
-  const [history, setHistory] = useState<Message[][]>([])
+  const [chat, setChat] = useState<Message[]>([]);
+  const [history, setHistory] = useState<Message[][]>([]);
   const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
+  const [answer, setAnswer] = useState("")
 
   useEffect(() => {
     const fetchChats = async () => {
       try {
-        const res = await fetch("/api/chat_history")
+        const res = await fetch("http://localhost:3001/api2/chat-history");
         if (res.ok) {
-          const chats = await res.json()
-          const [firstChat, ...rest] = chats
+          const chats = await res.json();
+          const [firstChat, ...rest] = chats;
           const { chat_history } = firstChat;
-          setHistory(chat_history)
+          setHistory(chat_history);
         } else {
-          throw new Error("Request failed")
+          throw new Error("Request failed");
         }
       } catch (e) {
-        console.log('Error: ', e)
+        console.log("Error: ", e);
       }
-    }
+    };
     fetchChats();
-  }, [])
+  }, []);
 
   useEffect(() => {
     const chat_history = history;
-    setHistory(chat_history)
+    setHistory(chat_history);
     const chatHistory = chat_history.flat();
-    setChat(chatHistory) 
-  },[history])
+    setChat(chatHistory);
+  }, [history]);
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
-  }
+  };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    setIsLoading(true)
     e.preventDefault();
-    console.log(history)
-  
     const body = JSON.stringify({
       message,
       history: history,
-      SystemMessage: "Act like a helpful assistant"
-    })
-    const res = await fetch("/api/chat", {
+      SystemMessage: "Act like a helpful assistant",
+    });
+
+    let aiResponse = "";
+
+    const res = await fetch("http://localhost:3001/api2/chat", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Transfer-Encoding":'chunked',
       },
       body,
-    })
+    });
 
-    if (!res.ok) throw new Error("Could not send message")
+    if (!res.ok) throw new Error("Could not send message");
 
-    const {response} = await res.json();
-    console.log({response})
+    const reader = res.body?.getReader();
+    if (reader) {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          break;
+        }
+        aiResponse += new TextDecoder().decode(value);
+        setAnswer(aiResponse)
+      }
+    }
 
-    
+    setIsLoading(false)
+
     const newMessages: Message[] = [
       {
         type: "USER",
@@ -71,31 +86,33 @@ export default function Chat() {
       },
       {
         type: "AI",
-        message: response,
-        createdAt: Date.now()
-      }
-    ]
+        message: aiResponse,
+        createdAt: Date.now(),
+      },
+    ];
 
-    setHistory((prev) => [...prev, newMessages])
+    setHistory((prev) => [...prev, newMessages]);
+    setAnswer("")
   };
+
 
   return (
     <section className="flex flex-col w-full py-8 px-4">
       <div className="flex flex-col gap-4 w-full md:max-w-[800px] mx-auto">
-        {chat.filter(message => message.type !== "SYSTEM").map((message, index) => {
-          return (
-            <ChatBubble message={message} key={`${index}`}/>
-          )
-        })}
-        
+        {chat
+          .filter((message) => message.type !== "SYSTEM")
+          .map((message, index) => {
+            return <ChatBubble message={message} key={`${index}`} />;
+          })}
+        {isLoading && <ChatBubble message={{type:"AI", message: answer, createdAt: Date.now()}} /> }
         <form className="flex gap-4" onSubmit={handleSubmit}>
           <textarea
             className="textarea textarea-bordered w-full resize-none"
             onChange={(e) => handleChange(e)}
             placeholder="Write your message here..."
           />
-          <button className="btn h-[100px]" type="submit" name="message">
-            <span className="loading loading-spinner"></span>
+          <button disabled={isLoading} className="btn h-[100px]" type="submit" name="message">
+            {isLoading && (<span className="loading loading-spinner"></span>)}
             Send
           </button>
         </form>
@@ -104,13 +121,13 @@ export default function Chat() {
   );
 }
 
-function ChatBubble({ message }: {message: Message }) {
-  const session = useSession()
+function ChatBubble({ message }: { message: Message }) {
+  const session = useSession();
   const aiImage = "https://picsum.photos/id/237/200/300";
   const defaultImage = "https://picsum.photos/id/252/200/300";
   const humanImage = session?.data?.user?.image ?? defaultImage;
   const userName = session?.data?.user?.name ?? "User";
-  const isAiMessage = message.type === "AI"
+  const isAiMessage = message.type === "AI";
   return (
     <>
       <div className={`chat ${isAiMessage ? "chat-start" : "chat-end"}`}>
@@ -126,7 +143,9 @@ function ChatBubble({ message }: {message: Message }) {
         </div>
         <div className="chat-header">
           {isAiMessage ? "AI" : userName}
-          <time className="text-xs opacity-50">{new Date(message.createdAt).toISOString()}</time>
+          <time className="text-xs opacity-50">
+            {new Date(message.createdAt).toISOString()}
+          </time>
         </div>
         <div className="chat-bubble">{message.message}</div>
         <div className="chat-footer opacity-50">Message Status</div>
