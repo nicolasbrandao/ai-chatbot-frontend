f78c3f072dd7520b5b2b1676043e285ed3149f00
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-09-02 18:29:17 -0300

    Added URL rewrite rule for '/api2/:path*' to next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api2/:path*",
+        destination: "http://localhost:3001/api2/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
@@ -9,7 +17,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-      }
+      },
     ],
   },
 };
