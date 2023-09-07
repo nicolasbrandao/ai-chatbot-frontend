b2e07fd67bccbfd7aa7bdf02d7b25e597edab10d
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2023-09-06 21:08:34 -0300

    FEA : Added new ChatPreview component
     - The component is responsible for showing the last three messages from the chat history
     - It makes use of a previously defined ChatBubble component to display individual messages
    
    this commit was generated by lguibr/comai

---
+++ app/components/ChatPreview.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Message } from "@/types/models/shared";
+import ChatBubble from "./ChatBuble";
+
+interface SimpleChatProps {
+  chat_history: Message[][];
+}
+
+const ChatPreview: React.FC<SimpleChatProps> = ({ chat_history }) => {
+  const flatChatHistory = chat_history.flat();
+  const lastThreeMessages = flatChatHistory.slice(-3);
+
+  return (
+    <div className="flex flex-col gap-4 w-full md:max-w-[800px] mx-auto">
+      {lastThreeMessages.map((message, index) => (
+        <ChatBubble message={message} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default ChatPreview;
