905fb0152cd8101fc24dd1517c69bde8bcacdff6
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-09-07 19:50:56 -0300

    FEA: add delete chat base button

--- app/components/ChatPreview.tsx
+++ app/components/ChatPreview.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Message } from "@/types/models/shared";
-import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 interface SimpleChatProps {
   chat_history: Message[][];
@@ -9,7 +9,6 @@ interface SimpleChatProps {
 const ChatPreview: React.FC<SimpleChatProps> = ({ chat_history }) => {
   const flatChatHistory = chat_history ? chat_history.flat() : [];
   const lastMessage = flatChatHistory.slice(-1);
-
   return (
     <div className="flex flex-col gap-4 w-full md:max-w-[800px] mx-auto">
       {lastMessage.map((message, index) => (
@@ -31,6 +30,18 @@ const PreviewCard = ({ message }: PreviewCardProps) => {
       <ChatBubbleLeftIcon className="h-5 w-5" />
       {/* TODO: Use a message title instead of spliting the message */}
       <p className="rounded">{message}</p>
+      <DeleteChatButton />
     </div>
+  );
+};
+
+const DeleteChatButton = () => {
+  return (
+    <button className="btn">
+      <TrashIcon
+        className="h-5 w-5 cursor-pointer"
+        onClick={(e) => e.stopPropagation()}
+      />
+    </button>
   );
 };
