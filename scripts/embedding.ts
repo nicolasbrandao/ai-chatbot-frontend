ed97ec2557cc59453b02f914d62ea9ec4c112b18
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2023-09-18 14:00:26 -0300

    ENH:clean project

--- scripts/embedding.ts
+++ scripts/embedding.ts
@@ -17,69 +17,64 @@ const splitter = new RecursiveCharacterTextSplitter({
   chunkOverlap: 0,
 });
 
-
 interface Embedding {
   content: string;
   embedding: number[];
-
+  metadata?: Record<string, any>;
 }
 
-const loadFiles = async (path : string) : Promise<Embedding[]>=> {
-  const loader = new DirectoryLoader(
-    path,
-    {
-      ".json": (path) => new JSONLoader(path, "/texts"),
-      ".jsonl": (path) => new JSONLinesLoader(path, "/html"),
-      ".txt": (path) => new TextLoader(path),
-      ".csv": (path) => new CSVLoader(path, "text"),
-      ".pdf": (path) => new PDFLoader(path),
-    }
-    );
-        const extractor = await pipeline(
-      "feature-extraction",
-      "Supabase/bge-small-en"
-    );
-    const docs = await loader.load();
-    const splitedDocs = await splitter.splitDocuments(docs);
-    let embeddings :Embedding[]= []
-    for (const document of splitedDocs) {
-      const content = document.pageContent;
+const loadFiles = async (path: string): Promise<Embedding[]> => {
+  const loader = new DirectoryLoader(path, {
+    ".json": (path) => new JSONLoader(path, "/texts"),
+    ".jsonl": (path) => new JSONLinesLoader(path, "/html"),
+    ".txt": (path) => new TextLoader(path),
+    ".csv": (path) => new CSVLoader(path, "text"),
+    ".pdf": (path) => new PDFLoader(path),
+  });
+  const extractor = await pipeline(
+    "feature-extraction",
+    "Supabase/bge-small-en"
+  );
+  const docs = await loader.load();
+  const splittedDocs = await splitter.splitDocuments(docs);
+  let embeddings: Embedding[] = [];
+  for (const splittedDocument of splittedDocs) {
+    const content = splittedDocument.pageContent;
 
-
-
-
-    const output: Tensor = await extractor(document.pageContent, {
+    const output: Tensor = await extractor(splittedDocument.pageContent, {
       pooling: "mean",
       normalize: true,
     });
 
     const embedding = output.flatten().tolist();
-    embeddings.push({content,embedding})
+    embeddings.push({
+      content,
+      embedding,
+      metadata: splittedDocument.metadata,
+    });
   }
-
 
   return embeddings;
-  }
-
+};
 
 const main = async () => {
   const folderPath = process.argv[2];
-  const savePath = process.argv[3] ?  path.join(folderPath, 'embeddings', process.argv[3]): path.join(folderPath, 'embeddings');
+  const savePath = process.argv[3]
+    ? path.join(folderPath, "embeddings", process.argv[3])
+    : path.join(folderPath, "embeddings");
 
   if (!folderPath) {
     console.error("Please provide a folder path.");
     process.exit(1);
   }
 
-  // Create 'embeddings' folder if it doesn't exist
   if (!fs.existsSync(savePath)) {
     fs.mkdirSync(savePath, { recursive: true });
   }
 
   const embeddings = await loadFiles(folderPath);
 
-  // Save the embeddings as a JSON file
-  const jsonFilePath = path.join(savePath, 'embeddings.json');
+  const jsonFilePath = path.join(savePath, "embeddings.json");
   fs.writeFileSync(jsonFilePath, JSON.stringify(embeddings, null, 2));
 
   console.log(`Embeddings saved to ${jsonFilePath}`);
