cc54765cf741192de30e210dd7b04d9d4740fdb1
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-09-29 15:40:01 -0300

    FEA: create functions for redirecting users

--- apps/web/shared/auth.ts
+++ apps/web/shared/auth.ts
@@ -1,11 +1,27 @@
 import GoogleProvider from "next-auth/providers/google";
-import { AuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
+import { headers } from "next/headers";
 
 const googleConfig = GoogleProvider({
   clientId: process.env.GOOGLE_CLIENT_ID ?? "",
   clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
 });
 
-const authConfig: AuthOptions = { providers: [googleConfig] };
+const authConfig: NextAuthOptions = {
+  providers: [googleConfig],
+};
 
 export default authConfig;
+
+export function getOriginPath() {
+  const headerObj = headers();
+  const origin = headerObj.get(ORIGIN_URL_KEY) ?? "";
+  return origin;
+}
+
+export function makeRedirectURL(origin: string) {
+  return `${NO_SESSION_REDIRECT}${origin}`;
+}
+
+export const ORIGIN_URL_KEY = "x-url";
+const NO_SESSION_REDIRECT = "/auth/signin?callbackUrl=";
