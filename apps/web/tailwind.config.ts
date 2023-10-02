e863262f21a2388d6e0e69d5280b49f27e2a1623
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-10-02 15:24:02 -0300

    FIX: scrolling behaviour

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,15 +1,22 @@
 import type { Config } from "tailwindcss";
 import daisy from "daisyui";
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      height: {
+        "chat-height": "calc(100vh - 136px)",
+      },
+    },
+  },
   daisyui: {
     themes: ["light", "dark"],
   },
-  plugins: [daisy],
+  plugins: [daisy, require("tailwind-scrollbar")],
 };
 export default config;
